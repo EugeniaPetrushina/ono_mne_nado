f91e0b5375c469fbfd17ec1a67ec6a8ac417cda6
EugeniaPetrushina <cats53e@gmail.com>
2022-05-24 22:55:55 +0300

    это для бота

---
+++ main.py
@@ -0,0 +1,22 @@
+import bs4
+from io import BytesIO
+import random
+import requests
+
+
+def random_game():
+    page = 1
+    # req_game = requests.get("https://stopgame.ru/games?p=" + str(page))
+    # html = bs4.BeautifulSoup(r.content, "html.parser")
+    #url = "https://randomgeeks.ru/generator-games/random/"
+    #result_find = soup.find('section', class_="generator").find('div', class_="generator-box")
+
+    url = "https://store.steampowered.com/explore/random/"
+    req_game = requests.get(url)
+    soup = bs4.BeautifulSoup(req_game.text, "html.parser")
+
+    name = soup.find('div', id="appHubAppName").getText()
+
+    print(name)
+
+random_game()
