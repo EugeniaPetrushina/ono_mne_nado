4786cc55f3fc3f9409c1631a60bc73b5f7c0ab12
EugeniaPetrushina <cats53e@gmail.com>
2022-05-25 14:28:29 +0300

    аааааааааааааааааааааа

--- main.py
+++ main.py
@@ -5,19 +5,16 @@ import requests
 
 
 def random_game():
-    page = 1
-    # req_game = requests.get("https://stopgame.ru/games?p=" + str(page))
-    # html = bs4.BeautifulSoup(r.content, "html.parser")
-    #url = "https://randomgeeks.ru/generator-games/random/"
-    #result_find = soup.find('section', class_="generator").find('div', class_="generator-box")
 
     url = "https://store.steampowered.com/explore/random/"
     req_game = requests.get(url)
     soup = bs4.BeautifulSoup(req_game.text, "html.parser")
 
     name = soup.find('div', id="appHubAppName").getText()
-    prise = soup.find('div', class_="game_purchase_price price").getText()
+    prise = soup.find('div', class_="game_purchase_price price").getText().strip()
+    genre = soup.find('div', id="developers_list").getText().strip()
     print(name)
     print(prise)
+    print(genre)
 
 random_game()
